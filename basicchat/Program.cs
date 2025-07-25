24d1827a839a6acedf84bb0f8f16b74dbe7b248c
Michael Shin <mshin1303@gmail.com>
2025-07-25 13:44:40 +1000

    add: basicchat

---
+++ basicchat/Program.cs
@@ -0,0 +1,13 @@
+﻿
+using Microsoft.Extensions.AI;
+
+// dotnet add package Microsoft.Extensions.AI.Ollama --version 9.7.1
+
+IChatClient client =
+    new OllamaChatClient(new Uri("http://localhost:11434/"), "phi4-mini");
+
+var response = client.GetStreamingResponseAsync("What is .NET?");
+await foreach (var item in response)
+{
+    Console.Write(item);
+}
