66b76f2b77a18c02fe71ca563d32cbbf14fb8ef2
Michael Shin <mshin1303@gmail.com>
2025-07-25 13:51:38 +1000

    add: sentiment analysis

--- basicchat/Program.cs
+++ basicchat/Program.cs
@@ -1,13 +1,21 @@
 ﻿
 using Microsoft.Extensions.AI;
+using System.Text;
+
 
 // dotnet add package Microsoft.Extensions.AI.Ollama --version 9.7.1
 
 IChatClient client =
     new OllamaChatClient(new Uri("http://localhost:11434/"), "phi4-mini");
 
-var response = client.GetStreamingResponseAsync("What is .NET?");
-await foreach (var item in response)
-{
-    Console.Write(item);
-}
+StringBuilder prompt = new StringBuilder();
+prompt.AppendLine("You will analyze the sentiment of the following product reviews. Each line is its own review. Output the sentiment of each review in a bulleted list and then provide a generate sentiment of all reviews. ");
+prompt.AppendLine("I bought this product and it's amazing. I love it!");
+prompt.AppendLine("This product is terrible. I hate it.");
+prompt.AppendLine("I'm not sure about this product. It's okay.");
+prompt.AppendLine("I found this product based on the other reviews. It worked for a bit, and then it didn't.");
+
+// send the prompt to the model and wait for the text completion
+var response = await client.GetResponseAsync(prompt.ToString());
+
+Console.WriteLine(response.Text);
